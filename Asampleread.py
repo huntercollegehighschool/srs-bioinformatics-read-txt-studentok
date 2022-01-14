2508fe135407c77e86f48eef718a5335dfe3d3a6
studentok <74572678+studentok@users.noreply.github.com>
2022-01-14 17:32:29 +0000

    Done some work on this

--- Asampleread.py
+++ Asampleread.py
@@ -1,3 +1,7 @@
 """
 Write a program (doesn't have to be a function, but can be) that reads what's in sample.txt and prints it to the console.
 """
+textfile = open("sample.txt", "r")
+content = textfile.read()
+print(content)
+ 
--- Baminoacids.py
+++ Baminoacids.py
@@ -5,4 +5,15 @@ weightedstring should read from masstable.txt. It's helpful to have those masses
 """
 
 def weightedstring(protein):
-  pass
+  table = open('masstable.txt', 'r')
+  table_list = table.read().split()
+  counter = 1
+  table_dict = {}
+  for element in table_list:
+    if counter % 2 == 0:
+      table_dict.setdefault(element, 0)
+    else: 
+      prev_element = table_list[counter-2]
+      table_dict.setvalue(prev_element, element) 
+  print(table_dict)
+  
